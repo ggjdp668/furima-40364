97bc90955e9f725fefc72eb7bea729930f2f0173
ggjdp668 <ggjdp668@gmail.com>
2024-03-10 19:10:44 +0900

    itemsテーブルの作成,バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,19 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :shipping_fee_bearer_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :image,                  presence: true
+  validates :item_name,              presence: true
+  validates :description,            presence: true
+  validates :category_id,            presence: true
+  validates :condition_id,           presence: true
+  validates :shipping_fee_bearer_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :shipping_day_id,        presence: true
+  validates :price,                  presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
+end
