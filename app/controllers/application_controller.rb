04719c4164c1cfad4dd30216275d15e6eac7671a
ggjdp668 <ggjdp668@gmail.com>
2024-03-10 11:14:04 +0900

    要修正箇所の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-    def configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth])
   end
 end
