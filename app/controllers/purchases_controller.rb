4a6e8ba21b4131b9acd4df52638d228b3fab28aa
ggjdp668 <ggjdp668@gmail.com>
2024-03-14 16:32:06 +0900

    purchasesテーブルの作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,23 @@ class PurchasesController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
+  end
+  
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
   end
 
+  private
+
+  def purchase_params
+    params.require(:purchase).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
