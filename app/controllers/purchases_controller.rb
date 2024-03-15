de8ced009e961fa51b9f732d3c1ac01c813b31d8
ggjdp668 <ggjdp668@gmail.com>
2024-03-15 17:13:37 +0900

    addressesテーブル,PurchaseAddressクラスの作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,14 @@
 class PurchasesController < ApplicationController
-
   def index
     @item = Item.find(params[:item_id])
-    @purchase = Purchase.new
+    @purchase_address = PurchaseAddress.new
   end
   
   def create
     @item = Item.find(params[:item_id])
-    @purchase = Purchase.new(purchase_params)
-    if @purchase.valid?
-      @purchase.save
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
       return redirect_to root_path
     else
       render 'index', status: :unprocessable_entity
@@ -17,8 +16,7 @@ class PurchasesController < ApplicationController
   end
 
   private
-
   def purchase_params
-    params.require(:purchase).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
